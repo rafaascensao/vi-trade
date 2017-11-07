77e7a098b51a3e21a26e33ed16ed87e3059bf079
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2017-11-07 19:09:55 +0000

    changed some things and added structure+

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vi-trade
 Academic project for the course Information Visualization
+
+Run (python3) on main project folder:
+```sh
+python -m http.server 8888
+```
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
- var map = new Datamap({
-   element: document.getElementById('container'),
-   responsive: true
- });
+//Include html
+w3.includeHTML();
 
-window.addEventListener('resize', function() {
-  map.resize();
+// MAIN INSIDE THIS FUNCTION ONLY
+$(document).ready(function(){
+  //Removes loading
+  $('.loader').addClass('hidden')
+
+
+  var map = new Datamap({
+     element: document.getElementById('map'),
+     responsive: true
+   });
+   map.arc([{
+     origin: {
+       latitude: 61,
+       longitude: -149
+     },
+     destination: {
+       latitude: -22,
+       longitude: -43
+     }
+   }], {
+     greatArc: true,
+     animationSpeed: 2000
+   });
+
+
+  window.addEventListener('resize', function() {
+    map.resize();
+  });
+
+
+
+
 });
