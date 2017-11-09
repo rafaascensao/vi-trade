fb8f8d638616d00792c313e5010e8f3d3e5877bc
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2017-11-09 20:51:27 +0000

    trying limiting pan zoom map

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,18 @@ $(document).ready(function(){
            zoom = d3.behavior.zoom().scaleExtent([1, 30]).on("zoom",redraw)
            datamap.svg.call(zoom);
            function redraw() {
+             console.log(d3.event.translate)
+             if(d3.event.translate[0] > 0)
+              d3.event.translate[0] = -1
+             if(d3.event.translate[1] > 0)
+              d3.event.translate[1] = -1
+             /*console.log("COMPARING WITH "+ d3.event.translate[0] + " " + d3.event.scale)
+             if(d3.event.translate[0] < d3.event.scale)
+              d3.event.translate[0] = -1 * d3.event.scale
+             if(d3.event.translate[1] < d3.event.scale)
+              d3.event.translate[1] = -1 * d3.event.scale*/
+            zoom.translate(d3.event.translate)
+             if(d3.event)
              datamap.svg.selectAll("g").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
            }
            datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
