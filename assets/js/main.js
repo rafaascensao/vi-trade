c3fef990a890c7378205f5eac2adb82154702ed7
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2017-11-28 17:33:59 +0000

    cloropleth map working partiall

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 // Trade dataset
 var dataset;
 var countries_map;
+var year = 2004;
+var countries;
+var products = ["Textiles and Clothing","Wood","Minerals","Food Products", "Chemicals", "Plastic or Rubber","Animal", "Fuels", "Mach and Elec"];
+var globalProducts;
+var countriesCodes = {};
+var productsColors = [[42,147,0],[102,51,0],[102,204,255],[0,51,153],[255,255,0],[112,48,160],[192,0,0],[255,153,0],[255,153,255]]
 //Include html
 w3.includeHTML(startViews);
 
 // MAIN INSIDE THIS FUNCTION ONLY
 function startViews(){
   //Removes loading
-  $('.loader').addClass('hidden')
-//  open();
+  open();
   startTimeline()
 
   startMap()
@@ -22,9 +27,37 @@ function startViews(){
 /* Open file and shit */
 function open(){
   d3.csv("../../dataGather/exports.csv", function(data){
-    console.log(data[0]);
     dataset = data;
+    generateCountriesList();
+    generateCodesDic();
+    globalProducts = getSumProducts(products);
+    $('.loader').addClass('hidden')
+    $('.row').removeClass('hidden')
   })
+}
+function generateCodesDic(){
+  var codes;
+  d3.csv("../../dataGather/countryCodes.csv", function(data){
+    codes = data;
+      countries.forEach(function(country){
+        code = codes.filter(function(el){ return el["Name"] == country})
+        countriesCodes[country] = code[0]["Code"];
+      })
+  })
+}
+function generateCountriesList(){
+  x = []
+  dataset.filter(function(element){
+    if( x.indexOf(element["Reporter Name"]) < 0){
+		x.push(element["Reporter Name"])
+    }
+  })
+  dataset.filter(function(element){
+    if( x.indexOf(element["Partner Name"]) < 0){
+		x.push(element["Partner Name"])
+    }
+  })
+  countries = x
 }
 function startTimeline(){
   $('.timeline .options p').click(toggleButtons);
@@ -33,19 +66,20 @@ function startTimeline(){
       range: "max",
       min: 1989,
       max: 2015,
-      value: 2,
+      value: year,
       slide: function( event, ui ) {
         $( "#amount" ).val( ui.value );
         if(ui.value == 1989 || ui.value == 2015)
           $('.timeline .slider .slide span .year').text('')
         else
           $('.timeline .slider .slide span .year').text(ui.value);
-
+        year = ui.value
       }
     });
     $('.timeline .slider .slide').append("<div class='left-slide'><div class='min-max-year'>1989</div></div><div class='right-slide'><div class='min-max-year'>2015</div></div>")
     $('.timeline .slider .slide span').append("<div class='year'></div>")
     $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
+    $('.timeline .slider .slide span .year').text(year+'')
 
 
   function toggleButtons(){
@@ -59,10 +93,9 @@ function startTimeline(){
     }
   }
   function selectProduct(){
-    console.log($(this))
     $(this).parent().children().removeClass('selected')
     $(this).addClass('selected')
-
+    fillCloropleth($(this).attr('product'))
   }
 
 }
@@ -75,6 +108,7 @@ function toggleFlowChoroplethMap(choropleth_map=false,flow_map=true){
          scope:'world',
          element: document.getElementById('map'),
          responsive: true,
+
          //projection: 'mercator',
          done: function(datamap) {
 
@@ -161,10 +195,207 @@ function zoomToArea(area,scale){
 
 
 
+
 }
 function startMap(){
   toggleFlowChoroplethMap(true,false)
   window.addEventListener('resize', function() {
     countries_map.resize();
   });
+}
+
+function getSumProducts(listProducts){
+  // QUERY
+  all = {}
+  listProducts.forEach(function(product){
+    console.log("Computing " + product)
+    count = {}
+    countries.forEach(function(country){
+      results = dataset.filter(function(element){
+        return element["Product Group"] == product && element["Reporter Name"] == country; })
+
+      soma = 0
+      results.forEach(function(element){
+        soma += parseFloat(element[year]);
+      })
+      count[country] = soma
+    })
+    all[product] = count
+  })
+  return all
+}
+
+function computeQuintiles(product){
+  x = []
+  countries.forEach(function(count){
+     x.push(globalProducts[product][count])})
+
+  x = x.sort(function(a,b){ return a-b})
+  step = Math.floor(x.filter(function(el){ return el != 0 }).length*0.25)
+
+  return step
+}
+
+/* generate cloropleth map */
+function fillCloropleth(product){
+  dic = { }
+
+  step = computeQuintiles(product)
+
+  x = []
+  countries.forEach(function(count){
+     x.push(globalProducts[product][count])})
+  x = x.sort(function(a,b){ return a-b})
+  no_zeros = x.filter(function(el){ return el != 0 })
+
+  firstV = no_zeros[0]
+  secondV = no_zeros[step-1]
+  thirdV = no_zeros[(step-1)*2]
+  fourthV = no_zeros[(step-1)*3]
+  fifthV = no_zeros[no_zeros.length - 1]
+
+
+  cor = productsColors[products.indexOf(product)]
+
+  countries.forEach(function(count){
+    value = globalProducts[product][count]
+    if (value == 0){
+      dic[countriesCodes[count]] = "rgba(129,129,130,1)"
+    }else if(value < secondV){
+      dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",0.4)"
+    }
+
+    else if(value < thirdV){
+      dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",0.6)"
+    }
+
+    else if(value < fourthV){
+      dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",0.8)"
+    }
+
+    else if(value < fifthV){
+      dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",1)"
+    }
+  })
+
+  opacity = 0.4
+  $('#bar-cloropleth > div').each(function(){
+    $(this).css('background',"rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ","+opacity+")")
+    opacity += 0.2
+  })
+
+  countries_map.updateChoropleth(dic)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* BARCHART */
+function startBarchart(){
+  var categories= ['','Accessories', 'Audiophile', 'Camera & Photo', 'Cell Phones', 'Computers','eBook Readers','Gadgets','GPS & Navigation','Home Audio','Office Electronics','Portable Audio','Portable Video','Security & Surveillance','Service','Television & Video','Car & Vehicle'];
+  var dollars = [213,209,190,179,156,209,190,179,213,209,190,179,156,209,190,190];
+  var colors = ['#0000b4','#0082ca','#0094ff','#0d4bcf','#0066AE','#074285','#00187B','#285964','#405F83','#416545','#4D7069','#6E9985','#7EBC89','#0283AF','#79BCBF','#99C19E'];
+
+  var grid = d3.range(25).map(function(i){
+    return {'x1':0,'y1':0,'x2':0,'y2':'480'};
+  });
+
+  var tickVals = grid.map(function(d,i){
+    if(i>0){ return i*10; }
+    else if(i===0){ return "100";}
+  });
+
+  var xscale = d3.scale.linear()
+          .domain([10,250])
+          .range([0,722]);
+
+  var yscale = d3.scale.linear()
+          .domain([0,categories.length])
+          .range([0,480]);
+
+  var colorScale = d3.scale.quantize()
+          .domain([0,categories.length])
+          .range(colors);
+
+  var canvas = d3.select('#bar_chart svg');
+
+var grids = canvas.append('g')
+          .attr('id','grid')
+          .attr('transform','translate(50,10)')
+          .selectAll('line')
+          .data(grid)
+          .enter()
+          .append('line')
+          .attr({'x1':function(d,i){ return i*30; },
+             'y1':function(d){ return d.y1; },
+             'x2':function(d,i){ return i*30; },
+             'y2':function(d){ return d.y2; },
+          })
+          .style({'stroke':'#adadad','stroke-width':'1px'});
+
+var	xAxis = d3.svg.axis();
+  xAxis
+    .orient('bottom')
+    .scale(xscale)
+    .tickValues(tickVals);
+
+var	yAxis = d3.svg.axis();
+  yAxis
+    .orient('left')
+    .scale(yscale)
+    .tickSize(2)
+    .tickFormat(function(d,i){ return categories[i]; })
+    .tickValues(d3.range(17));
+
+var y_xis = canvas.append('g')
+          .attr("transform", "translate(50,0)")
+          .attr('id','yaxis')
+          .call(yAxis);
+
+var x_xis = canvas.append('g')
+          .attr("transform", "translate(50,480)")
+          .attr('id','xaxis')
+          .call(xAxis);
+
+var chart = canvas.append('g')
+          .attr("transform", "translate(50,0)")
+          .attr('id','bars')
+          .selectAll('rect')
+          .data(dollars)
+          .enter()
+          .append('rect')
+          .attr('height',19)
+          .attr({'x':0,'y':function(d,i){ return yscale(i)+19; }})
+          .style('fill',function(d,i){ return colorScale(i); })
+          .attr('width',function(d){ return 0; });
+
+
+var transit = d3.select("svg").selectAll("rect")
+            .data(dollars)
+            .transition()
+            .duration(1000)
+            .attr("width", function(d) {return xscale(d); });
+
+var transitext = d3.select('#bars')
+          .selectAll('text')
+          .data(dollars)
+          .enter()
+          .append('text')
+          .attr({'x':function(d) {return xscale(d)-200; },'y':function(d,i){ return yscale(i)+35; }})
+          .text(function(d){ return d+"$"; }).style({'fill':'#fff','font-size':'14px'});
+
+
+
 }
