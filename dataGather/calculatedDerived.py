a9d8653bf89b392169316f37985b2993cb4d0a09
Luis Neto <luis.ms.neto@gmail.com>
2017-11-28 15:23:02 +0000

    calculatedDerived updated.

--- dataGather/calculatedDerived.py
+++ dataGather/calculatedDerived.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import csv
 
-dfImport = pd.read_csv('imports.csv')
+#dfImport = pd.read_csv('imports.csv')
 dfExport = pd.read_csv('exports.csv')
 dfCountries = pd.read_csv('countryCodes.csv')
 countries = dfCountries['Name']
@@ -13,6 +13,14 @@ def calculateSumForReporter(rep, data):
     for year in years:
         res.append(allYears[year].sum())
     return res
+
+def calculateSumForPartner(rep, data):
+    res =[]
+    allYears = data.loc[(data['Partner Name'] == rep) & (data['Product Group'] == '  All Products')]
+    for year in years:
+        res.append(allYears[year].sum())
+    return res
+
 
 def sumRow(row):
     total = 0
@@ -29,7 +37,7 @@ with open('derived.csv', 'w') as fimp:
     for country in countries:
         print('Import: ' + country)
         myList = [country, 'Import']
-        sums = calculateSumForReporter(country, dfImport)
+        sums = calculateSumForReporter(country, dfExport)
         myList.extend(sums)
         myList.append(sumRow(sums))
         wr.writerow(myList) 
