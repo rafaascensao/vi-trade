23f2a522e10fa50823f5de607cdb3bece09db195
rafaascensao <rafascen@gmail.com>
2017-11-29 17:52:20 +0100

    Fixed fuels

--- dataGather/calculatedDerived.py
+++ dataGather/calculatedDerived.py
@@ -3,7 +3,7 @@ import csv
 
 #dfImport = pd.read_csv('imports.csv')
 dfExport = pd.read_csv('exports.csv')
-products = set(['All Products','Animal','Textiles and Clothing','Wood','Minerals','Food Products','Chemicals','Plastic or Rubber','Fuel','Mach and Elec'])
+products = set(['All Products','Animal','Textiles and Clothing','Wood','Minerals','Food Products','Chemicals','Plastic or Rubber','Fuels','Mach and Elec'])
 dfCountries = pd.read_csv('countryCodes.csv')
 countries = dfCountries['Name']
 years = ['1989','1990','1991','1992','1993','1994','1995','1996','1997','1998','1999','2000','2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015']
