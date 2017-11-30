e4728d990dbc0dd7b3250f94aad6c5e4d9f62c14
Gonçalo Fialho <goncalo.f.pires@hotmail.com>
2017-11-30 00:46:20 +0000

    bar chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,7 @@ function generateCodesDic(){
       code = codes.filter(function(el){ return el["Name"] == country})
       countriesCodes[country] = code[0]["Code"];
     })
+
   })
 }
 function generateCountriesList(){
@@ -77,6 +78,7 @@ function startTimeline(){
         $('.timeline .slider .slide span .year').text(ui.value);
       year = ui.value
       fillCloropleth(getSelectedProduct())
+      refreshBarChart()
     }
   });
   $('.timeline .slider .slide').append("<div class='left-slide'><div class='min-max-year'>1989</div></div><div class='right-slide'><div class='min-max-year'>2015</div></div>")
@@ -98,6 +100,7 @@ function startTimeline(){
     $(this).parent().children().removeClass('selected')
     $(this).addClass('selected')
     fillCloropleth($(this).attr('product'))
+    refreshBarChart()
   }
 
 }
@@ -235,7 +238,9 @@ function getSumProducts(listProducts){
 function computeQuintiles(product){
   x = []
   countries.forEach(function(count){
-    x.push(globalProducts[product][count][year])})
+    if (count != " World" && count != "European Union"){
+    x.push(globalProducts[product][count][year])}
+  })
 
   x = x.sort(function(a,b){ return a-b})
   step = Math.floor(x.filter(function(el){ return el != 0 }).length*0.25)
@@ -251,7 +256,9 @@ function fillCloropleth(product){
 
   x = []
   countries.forEach(function(count){
-    x.push(globalProducts[product][count][year])})
+    if (count != " World" && count != "European Union"){
+    x.push(globalProducts[product][count][year])}
+  })
   x = x.sort(function(a,b){ return a-b})
   no_zeros = x.filter(function(el){ return el != 0 })
 
@@ -275,10 +282,12 @@ function fillCloropleth(product){
   cor = productsColors[products.indexOf(product)]
 
   countries.forEach(function(count){
+    if (count != " World" && count != "European Union"){
     value = globalProducts[product][count][year]
     if (value == 0){
       dic[countriesCodes[count]] = "rgba(129,129,130,1)"
-    }else if(value < secondV){
+    }
+    else if(value < secondV){
       dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",0.4)"
     }
 
@@ -290,9 +299,10 @@ function fillCloropleth(product){
       dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",0.8)"
     }
 
-    else if(value < fifthV){
+    else if(value > fourthV){
       dic[countriesCodes[count]] = "rgba(" +cor[0]+ "," +cor[1]+ "," +cor[2]+ ",1)"
     }
+  }
   })
 
   opacity = 0.2
@@ -329,22 +339,43 @@ function resetAllColors(){
 
 
 
+function getCountryExport(top){
+  //associative array=> country_key: export_value
+  cur_product = getSelectedProduct()
+  var country_export={};
+  countries.forEach(function(c){
+    if (c != " World" && c != "European Union"){
+      country_export[c]=globalProducts[cur_product][c][year]
+    }
+  })
+  //sorts the array by value
+  var items = Object.keys(country_export).map(function(key) {
+    return [key, country_export[key]];
+});
 
-
+// Sort the array based on the second element
+items.sort(function(first, second) {
+    return second[1] - first  [1];
+});
+  //returns the first 15 elements of an array
+  return items.slice(0,top)
+}
 
 /* BARCHART */
 
+
+
 /*Simple Bar Chart*/
 function barchart() {
-  var svg = d3.select("svg"),
+  var svg = d3.select("#bar_chart svg"),
       margin = {top: 20, right: 20, bottom: 30, left: 80},
       width = +svg.attr("width") - margin.left - margin.right,
       height = +svg.attr("height") - margin.top - margin.bottom;
 
   var tooltip = d3.select("body").append("div").attr("class", "toolTip");
 
-  var x = d3.scaleLinear().range([0, width]);
-  var y = d3.scaleBand().range([height, 0]);
+  var x = d3.scale.linear().range([0, width]);
+  var y = d3.ordinal().range([height, 0]);
 
   var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -383,38 +414,63 @@ function barchart() {
 };
 
 
-/*
+
+function refreshBarChart(){
+  $('#bar_chart svg > *').remove();
+  startBarchart()
+}
+
 function startBarchart(){
-  var categories= ['','Accessories', 'Audiophile', 'Camera & Photo', 'Cell Phones', 'Computers','eBook Readers','Gadgets','GPS & Navigation','Home Audio','Office Electronics','Portable Audio','Portable Video','Security & Surveillance','Service','Television & Video','Car & Vehicle'];
-  var dollars = [213,209,190,179,156,209,190,179,213,209,190,179,156,209,190,190];
+ var l = getCountryExport(15)
+ categories = ['']
+ dollars = []
+  l.forEach(function(element){
+  	categories.push(element[0])
+  	dollars.push(Math.floor(parseFloat(element[1])/1000000))
+  });
+
+//  var categories= ['','Accessories', 'Audiophile', 'Camera', 'Cell Phones', 'Computers','eBook Readers','Gadgets','GPS ','Home Audio','Office','Portable Audio','Portable Video','Security ','Service','Television ','Car & Vehicle'];
+//  var dollars = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
   var colors = ['#0000b4','#0082ca','#0094ff','#0d4bcf','#0066AE','#074285','#00187B','#285964','#405F83','#416545','#4D7069','#6E9985','#7EBC89','#0283AF','#79BCBF','#99C19E'];
 
-  var grid = d3.range(25).map(function(i){
-    return {'x1':0,'y1':0,'x2':0,'y2':'480'};
+  /* Y2 ALTURA DO GRAFICO */
+  var grid = d3.range(8).map(function(i){
+    return {'x1':0,'y1':0,'x2':0,'y2':$('.bar-chart_container').height() - 10 };
   });
 
   var tickVals = grid.map(function(d,i){
-    if(i>0){ return i*10; }
-    else if(i===0){ return "100";}
+    /*if(i>0){ return i*22; }
+    else if(i===0){ return "100";}*/
+    return 0
   });
 
-  var xscale = d3.scale.linear()
-    .domain([10,250])
-    .range([0,722]);
 
+  /* DOMAIN [MININO , MAXIMO EXPORTADO]
+     RANGE [0, LARGURA DA DIV]
+  */
+  var xscale = d3.scale.linear()
+    .domain([1,dollars[0]])
+    .range([0,$('.bar-chart_container').width() - 110]);
+
+
+  /* DOMAIN [0, NUMERO PAISES]
+     RANGE [0, ALTURA DA DIV]
+  */
   var yscale = d3.scale.linear()
     .domain([0,categories.length])
-    .range([0,480]);
+    .range([0,$('.bar-chart_container').height() - 30]);
+
 
   var colorScale = d3.scale.quantize()
     .domain([0,categories.length])
     .range(colors);
 
   var canvas = d3.select('#bar_chart svg');
+  var tooltip = d3.select("#bar_chart").append("div").attr("class", "toolTip");
 
   var grids = canvas.append('g')
     .attr('id','grid')
-    .attr('transform','translate(50,10)')
+    .attr('transform','translate(100,10)')
     .selectAll('line')
     .data(grid)
     .enter()
@@ -422,7 +478,7 @@ function startBarchart(){
     .attr({'x1':function(d,i){ return i*30; },
       'y1':function(d){ return d.y1; },
       'x2':function(d,i){ return i*30; },
-      'y2':function(d){ return d.y2; },
+      'y2':function(d){ return d.y2 - 35; },
     })
     .style({'stroke':'#adadad','stroke-width':'1px'});
 
@@ -430,6 +486,7 @@ function startBarchart(){
   xAxis
     .orient('bottom')
     .scale(xscale)
+    .tickSize(2)
     .tickValues(tickVals);
 
   var	yAxis = d3.svg.axis();
@@ -440,18 +497,22 @@ function startBarchart(){
     .tickFormat(function(d,i){ return categories[i]; })
     .tickValues(d3.range(17));
 
+
   var y_xis = canvas.append('g')
-    .attr("transform", "translate(50,0)")
+    .attr("transform", "translate(100,0)")
     .attr('id','yaxis')
     .call(yAxis);
 
   var x_xis = canvas.append('g')
-    .attr("transform", "translate(50,480)")
+    .attr("transform", "translate(100,450)")
     .attr('id','xaxis')
     .call(xAxis);
 
+  color = productsColors[products.indexOf(getSelectedProduct())]
+  opacity = 1.0
+  //rgbacolor = 'rgba('+color[0]+','+color[1]+','+color[2]+','+(opacity-=0.05)+')'
   var chart = canvas.append('g')
-    .attr("transform", "translate(50,0)")
+    .attr("transform", "translate(100,0)")
     .attr('id','bars')
     .selectAll('rect')
     .data(dollars)
@@ -459,8 +520,17 @@ function startBarchart(){
     .append('rect')
     .attr('height',19)
     .attr({'x':0,'y':function(d,i){ return yscale(i)+19; }})
-    .style('fill',function(d,i){ return colorScale(i); })
-    .attr('width',function(d){ return 0; });
+    .style('fill',function(d,i){ return 'rgba('+color[0]+','+color[1]+','+color[2]+','+(opacity-=0.02)+')'; })
+    .attr('width',function(d){ return 0; })
+    .on("mousemove", function(d){
+          val = Math.floor(parseFloat(getCountryExport(15)[dollars.indexOf(d)][1])/1000)
+            tooltip
+              .style("left", d3.event.pageX - 50 + "px")
+              .style("top", d3.event.pageY - 70 + "px")
+              .style("display", "inline-block")
+              .html(val+" US($) Millions");
+        })
+    		.on("mouseout", function(d){ tooltip.style("display", "none");});;
 
 
   var transit = d3.select("svg").selectAll("rect")
@@ -469,15 +539,16 @@ function startBarchart(){
     .duration(1000)
     .attr("width", function(d) {return xscale(d); });
 
+/*
   var transitext = d3.select('#bars')
     .selectAll('text')
     .data(dollars)
     .enter()
     .append('text')
-    .attr({'x':function(d) {return xscale(d)-200; },'y':function(d,i){ return yscale(i)+35; }})
+    .attr({'x':function(d) {return xscale(d)-100; },'y':function(d,i){ return yscale(i)+35; } })
     .text(function(d){ return d+"$"; }).style({'fill':'#fff','font-size':'14px'});
+*/
 
-
+  d3.select('#bar_chart svg #yaxis').selectAll('text').attr('font-size','12px')
 
 }
-*/
